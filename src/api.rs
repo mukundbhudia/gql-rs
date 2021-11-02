b6d249e87e06ea6496b42c435219d019801fc485
Mukund Bhudia <mukund.bhudia@nandos.co.uk>
2021-11-02 12:23:12 +0000

    feat: wired up basic query

--- src/api.rs
+++ src/api.rs
@@ -6,6 +6,15 @@ pub async fn start() -> tide::Result<()> {
     let mut server = tide::Server::new();
 
     server.at("/").get(hello);
+    server.at("/query").post(graphql_handler);
+    server.at("/graphiql").get(|_| async move {
+        let mut res = Response::new(StatusCode::Ok);
+        res.set_body(Body::from_string(playground_source(
+            GraphQLPlaygroundConfig::new("/query"),
+        )));
+        res.set_content_type(mime::HTML);
+        Ok(res)
+    });
 
     server.listen(format!("0.0.0.0:{}", port)).await?;
 
@@ -14,4 +23,24 @@ pub async fn start() -> tide::Result<()> {
 
 async fn hello(_req: Request<()>) -> tide::Result<Response> {
     Ok(Response::builder(tide::StatusCode::Ok).body("OK").build())
+}
+
+async fn graphql_handler(req: Request<()>) -> tide::Result {
+    use async_graphql::*;
+    use async_graphql_tide::{receive_request, respond};
+
+    struct Query;
+
+    #[Object]
+    impl Query {
+        async fn add(&self, a: i32, b: i32) -> i32 {
+            a + b
+        }
+    }
+
+    let gql_request = receive_request(req).await?;
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    let response = schema.execute(gql_request).await;
+
+    respond(response)
 }
